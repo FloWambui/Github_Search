b5b752eb3c1d22522eec18d785a055dfc556e618
FloWambui <gflorencewambui@gmail.com>
2022-04-19 11:06:19 +0300

    this commit will import files into the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
+import { NameResultComponent } from './components/name-result/name-result.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ResultsRepoComponent } from './components/results-repo/results-repo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'profile', pathMatch: 'full'},
+  {path: 'profile', component:ProfileComponent},
+  {path: 'name-result/:username', component: NameResultComponent},
+  {path: 'result-repo/:reponame', component: ResultsRepoComponent},
+  {path: '**', component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
